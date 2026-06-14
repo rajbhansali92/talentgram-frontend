50c91bfd1581131b67ea98b091c88450fdb86781
Raj R Bhansali <rajrbhansali@Rajs-MacBook-Pro.local>
2026-06-14 18:38:11 +0530

    Expose GET method on static fallback og-image route handler to support HTTP GET crawler requests

--- frontend/src/app/og-image/route.jsx
+++ frontend/src/app/og-image/route.jsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export default function Image() {
+export async function GET() {
     return new ImageResponse(
         (
             <div
